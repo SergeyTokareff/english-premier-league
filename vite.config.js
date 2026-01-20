7e4819ba934345bb155ed89ec09c2568c6ba3604
SergeyTokareff <sergey.v.tokareff@gmail.com>
2026-01-20 18:45:48 +0200

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "/" : "/english-premier-league/",
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
