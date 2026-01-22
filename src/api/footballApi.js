4bb7d688ad78c0f6321fbaf36876f26fc75387cc
SergeyTokareff <sergey.v.tokareff@gmail.com>
2026-01-22 21:00:02 +0200

    fix api

--- src/api/footballApi.js
+++ src/api/footballApi.js
@@ -1,19 +1,19 @@
-// НЕ axios з football-data.org напряму
+export const getMatches = async () => {
+  const res = await fetch(
+    `/api/apiHandler?path=competitions/PL/matches`
+  );
+
+  if (!res.ok) throw new Error("Failed to fetch matches");
+  const data = await res.json();
+  return data.matches;
+}
 
 export const getStandings = async () => {
   const res = await fetch(
     `/api/apiHandler?path=competitions/PL/standings`
   );
+
   if (!res.ok) throw new Error("Failed to fetch standings");
   const data = await res.json();
   return data.standings[0].table;
-};
-
-export const getMatches = async () => {
-  const res = await fetch(
-    `/api/apiHandler?path=competitions/PL/matches`
-  );
-  if (!res.ok) throw new Error("Failed to fetch matches");
-  const data = await res.json();
-  return data.matches;
-};
+}
